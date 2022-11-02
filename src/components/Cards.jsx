fb4d9441276fa458dc080b59a77efdb06c027f66
Shrief Essam <shriefessam1999@gmail.com>
2022-11-02 18:43:09 +0200

    add titles for the cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,13 +6,19 @@ import './main.css'
 function Cards(props){
     
     const cards = props.characters.map(card=>
-        <div title={card.alt} 
+        <div key={card.id} >
+            <div title={card.alt} 
             onClick={()=>props.onClick(card.id)} 
             className='card' 
-            key={card.id} 
             style={{backgroundImage : `url(${card.url})`}}
-        >    
+            >    
+            </div>
+            <div>
+                <p className='name'>{card.alt}</p>
+            </div>
+
         </div>
+        
     )
 
     return(
